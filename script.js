1b0aa0f4de427710fd8322da884668ab8744623d
Akash <akashpillai40@gmail.com>
2026-04-23 20:17:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+let codes = document.querySelectorAll(".code");
+
+//codes[0].focus();
+//add event
+codes.forEach((input, index) => {
+	input.addEventListener("input", (e) => {
+		let value = e.target.value;
+
+		if (!/^[0-9]$/.test(value)) {
+         input.value = "";
+          return;
+        }
+
+		//Jump to nxt box
+		if(value && index < codes.length - 1){
+			codes[index + 1].focus();
+		}
+	});
+	input.addEventListener("keydown", (e) => {
+
+    // BACKSPACE behavior
+    if (e.key === "Backspace") {
+
+      // if current box empty → go to previous
+      if (input.value === "" && index > 0) {
+        codes[index - 1].focus();
+      } else {
+        // delete current value
+        input.value = "";
+      }
+    }
+
+  });
+	
+})
